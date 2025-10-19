754e5d08a934b3ab66bfe71c01301b8bb9e3f3b0
RELMYMathieu <ninjacowzx@gmail.com>
2025-10-19 18:49:33 +0200

    TUFX compatibility without custom version

--- OfCourseIStillLoveYou/TrackingCamera.cs
+++ OfCourseIStillLoveYou/TrackingCamera.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using HullcamVDS;
 using OfCourseIStillLoveYou.Client;
-using OfCourseIStillLoveYou.TUFX;
 using UnityEngine;
 using UnityEngine.Rendering;
 
